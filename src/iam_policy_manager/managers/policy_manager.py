7dcd096e9ec02c876bebf4f0f1c5d46b90cdf05d
karthik <karthikkunchala2398@gmail.com>
2026-08-08 20:05:47 +0530

    added sync policy to aws function and tested

--- src/iam_policy_manager/managers/policy_manager.py
+++ src/iam_policy_manager/managers/policy_manager.py
@@ -4,9 +4,11 @@ import logging
 from src.iam_policy_manager.config.loader import load_config
 from src.iam_policy_manager.services.template_service import render_policy
 from src.iam_policy_manager.services.file_service import FileService
+from src.iam_policy_manager.services.aws_iam_service import AWSIAMService
 
 logger = logging.getLogger(__name__)
 
+aws_iam_service = AWSIAMService()
 
 class PolicyManager:
     """
@@ -51,3 +53,46 @@ class PolicyManager:
                     "Failed to process %s",
                     config_file.name
                 )
+
+            self.sync_policy_to_aws(policy)
+
+
+    def sync_policy_to_aws(self, policy) -> None:
+        """
+        Sync the generated policy to AWS IAM.
+
+        Args:
+            policy: The Policy object to sync.
+        """
+
+        if aws_iam_service.policy_exists(policy.policy_name):
+
+            logger.info(
+                "Policy '%s' exists in AWS IAM. Updating policy.",
+                policy.policy_name
+            )
+
+            new_version = aws_iam_service.create_policy_version(
+                policy.policy_name,
+                policy.document
+            )
+
+            logger.info(
+                "Created new policy version '%s' for policy '%s'",
+                new_version["PolicyVersion"]["VersionId"],
+                policy.policy_name
+            )
+
+        else:
+            logger.info(
+                "Policy '%s' does not exist in AWS IAM. Creating policy.",
+                policy.policy_name
+            )
+
+            created_policy = aws_iam_service.create_policy(policy)
+
+            logger.info(
+                "Created new policy '%s' with ARN '%s'",
+                created_policy["Policy"]["PolicyName"],
+                created_policy["Policy"]["Arn"]
+            )
--- src/iam_policy_manager/services/aws_iam_service.py
+++ src/iam_policy_manager/services/aws_iam_service.py
@@ -8,7 +8,19 @@ class AWSIAMService:
 
         self.client = boto3.client("iam")
 
-    def policy_exists(self, policy_arn: str) -> bool:
+        sts_client = boto3.client("sts")
+
+        self.account_id = sts_client.get_caller_identity()["Account"]
+
+    def get_policy_arn(self, policy_name: str) -> str:
+
+        return (
+            f"arn:aws:iam::{self.account_id}:policy/{policy_name}"
+            )
+
+    def policy_exists(self, policy_name: str) -> bool:
+
+        policy_arn = self.get_policy_arn(policy_name)
 
         try:
 
@@ -37,7 +49,9 @@ class AWSIAMService:
             Tags=policy.tags
         )
 
-    def get_default_policy_document(self, policy_arn):
+    def get_default_policy_document(self, policy_name: str):
+
+        policy_arn = self.get_policy_arn(policy_name)
 
         policy = self.client.get_policy(
             PolicyArn=policy_arn
@@ -54,9 +68,10 @@ class AWSIAMService:
 
     def create_policy_version(
         self,
-        policy_arn,
+        policy_name: str,
         document
     ):
+        policy_arn = self.get_policy_arn(policy_name)
 
         return self.client.create_policy_version(
 
